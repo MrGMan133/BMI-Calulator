04c3e866e316066c268b2c281a558e8ba2db085f
Olaf <o.galekop@gmail.com>
2018-02-01 14:17:38 +0100

    no message

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,14 +6,12 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import org.apache.log4j.xml.DOMConfigurator;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			DOMConfigurator.configure("log4j.xml");
 			Parent root = FXMLLoader.load(getClass().getResource("/view/BMICalc.fxml"));
 			Scene scene = new Scene(root,400,400);
 			primaryStage.setTitle("BMI Calculator");
